0803c9a5d156007ac678e62cfda3715cb34853bf
sergey <sl1ckypwnzip@gmail.com>
2024-02-20 17:16:29 +0300

    Добавьте файлы проекта.

---
+++ logs_a_u_oper_3.py
@@ -0,0 +1,14 @@
+﻿summa_min = int(input("Минимальная сумма инвестиций - "))
+Maikal = int(input("Сколько $ у Майкла - "))
+Ivan = int(input("Сколько $ у Ивана - "))
+if (Maikal > summa_min) and (Ivan > summa_min):
+    print (2)
+elif (Maikal>
+      summa_min) and (Ivan<summa_min):
+    print ("Maikal")
+elif (Maikal<summa_min) and (Ivan>summa_min):
+    print ("Ivan")
+elif (Maikal<=summa_min) and (Ivan<=summa_min) and ((Maikal+Ivan)>=summa_min):
+    print (1)
+elif (Maikal<=summa_min) and (Ivan<=summa_min) and ((Maikal+Ivan)<=summa_min):
+    print (0)
